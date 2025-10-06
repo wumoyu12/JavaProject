db3e28eac4accbabd01ea6d9997c9d87faeec7fe
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-10-06 07:55:27 -0400

    Create Main.java

---
+++ StudentAcademia/Main.java
@@ -0,0 +1,17 @@
+package StudentInfo;
+
+public class Main
+{
+
+	public static void main(String[] args)
+	{
+		CollectInfo student1 = new CollectInfo();
+		System.out.println("==========================================================");
+		CollectInfo student2 = new CollectInfo();
+		System.out.println("==========================================================");
+		CollectInfo student3 = new CollectInfo();
+		System.out.println("==========================================================");
+	}
+
+}
+
