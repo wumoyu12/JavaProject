218a487bca05c204055a6119643b60309c1da9d3
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-10-06 07:48:21 -0400

    Create EvenOdd.java

---
+++ IntroToJava/javaPack/EvenOdd.java
@@ -0,0 +1,26 @@
+package javaPack;
+
+import java.util.Scanner;
+
+public class EvenOdd
+{
+
+	public static void main(String[] args)
+	{
+		Scanner input = new Scanner(System.in);
+
+		System.out.print("Enter a number: ");
+		int number = input.nextInt();
+
+		if (number % 2 == 0)
+		{
+			System.out.println("The number is even");
+		}
+		else
+		{
+			System.out.println("The number is odd");
+		}
+		    
+	}
+
+}
