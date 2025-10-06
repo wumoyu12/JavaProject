1c46c4f0a670a7a2f6f4b849760345c374c4d6ca
wumoyu12 <147264549+wumoyu12@users.noreply.github.com>
2025-10-06 07:49:56 -0400

    Create PrintNumber.java

---
+++ IntroToJava/javaPack/PrintNumber.java
@@ -0,0 +1,17 @@
+package javaPack;
+
+public class PrintNumber
+{
+
+	public static void main(String[] args)
+	{
+		int num = 1;
+
+        while (num <= 10)
+        {
+            System.out.println(num);
+            num = num + 1;
+        }
+	}
+
+}
